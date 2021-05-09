0fe78154a6283f26a1c40c719f20096d1fc0480f
Mohamed Bechir Mejri <mohamed.bechir.mejri.dev@gmail.com>
2021-05-09 18:52:57 +0100

    keep computerchoiceand variables

---
+++ main.js
@@ -0,0 +1,11 @@
+let computerChoice;
+let playerChoice;
+let computerScore = 0;
+let playerScore = 0;
+
+function pickComputerChoice() {
+ const rockPaperScissors = ["rock", "paper", "scissors"];
+ computerChoice =
+   rockPaperScissors[(Math.random() * rockPaperScissors.length) | 0];
+ return computerChoice;
+}
