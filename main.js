42b77319db7602bd40a1dd054f182699f44c494c
Mohamed Bechir Mejri <mohamed.bechir.mejri.dev@gmail.com>
2021-05-09 18:54:04 +0100

    style:

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ let computerScore = 0;
 let playerScore = 0;
 
 function pickComputerChoice() {
- const rockPaperScissors = ["rock", "paper", "scissors"];
- computerChoice =
-   rockPaperScissors[(Math.random() * rockPaperScissors.length) | 0];
- return computerChoice;
+  const rockPaperScissors = ["rock", "paper", "scissors"];
+  computerChoice =
+    rockPaperScissors[(Math.random() * rockPaperScissors.length) | 0];
+  return computerChoice;
 }
