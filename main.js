068cf625c50fb7f8222895a2b0faf629fa584ea3
Mohamed Bechir Mejri <mohamed.bechir.mejri.dev@gmail.com>
2021-05-09 22:11:22 +0100

    add reusable functions

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let playerScore = 0;
 const buttons = document.querySelectorAll(".choices");
 const score = document.getElementById("score");
 let currentScore = score.innerText;
+let comments =
 function pickComputerChoice() {
   const rockPaperScissors = ["rock", "paper", "scissors"];
   computerChoice =
@@ -13,8 +14,55 @@ function pickComputerChoice() {
   return computerChoice;
 }
 
+function playerWins() {
+  console.log(`Your ${playerChoice} Beats His ${computerChoice}. Hurray!`);
+}
+
+function computerWins() {
+  console.log(`His ${computerChoice} Beats Your ${playerChoice}. Too Bad!`);
+}
+
+function tie() {
+  console.log(`It's a tie, both of you chose ${playerChoice}.`);
+}
+function result(playersChoice, computersChoice) {
+  if (playersChoice === computersChoice) {
+    tie();
+    return "tie";
+  } else {
+    switch (computersChoice) {
+      case "rock":
+        if (playersChoice === "paper") {
+          playerWins();
+          return "playerWins";
+        } else {
+          computerWins();
+          return "computerWins";
+        }
+      case "paper":
+        if (playersChoice === "scissors") {
+          playerWins();
+          return "playerWins";
+        } else {
+          computerWins();
+          return "computerWins";
+        }
+      case "scissors":
+        if (playersChoice === "rock") {
+          playerWins();
+          return "playerWins";
+        } else {
+          computerWins();
+          return "computerWins";
+        }
+    }
+  }
+}
+
 buttons.forEach((button) =>
   button.addEventListener("click", () => {
     playerChoice = button.value;
+    pickComputerChoice();
+    result(playerChoice, computerChoice);
   })
 );
