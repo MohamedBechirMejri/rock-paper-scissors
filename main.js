f669894b2f592a2d6fab1bde962c9e0ca37c496a
Mohamed Bechir Mejri <mohamed.bechir.mejri.dev@gmail.com>
2021-05-09 21:20:01 +0100

    feat:

--- main.js
+++ main.js
@@ -3,9 +3,18 @@ let playerChoice;
 let computerScore = 0;
 let playerScore = 0;
 
+const buttons = document.querySelectorAll(".choices");
+const score = document.getElementById("score");
+let currentScore = score.innerText;
 function pickComputerChoice() {
   const rockPaperScissors = ["rock", "paper", "scissors"];
   computerChoice =
     rockPaperScissors[(Math.random() * rockPaperScissors.length) | 0];
   return computerChoice;
 }
+
+buttons.forEach((button) =>
+  button.addEventListener("click", () => {
+    playerChoice = button.value;
+  })
+);
